60e578aed6c03f32cb8a3488921645b9397be7e3
etaiklein <etaiklein@gmail.com>
2013-06-25 19:03:50 -0300

    Updated Spacing

--- WordCheckPlugin.php
+++ WordCheckPlugin.php
@@ -1,53 +1,53 @@
 <?php
 
 class WordCheckPlugin extends MGWeightingPlugin{
-	public $enableOnInstall = true;
-	public $hasAdmin = true;
-	
-	function score(&$game, &$game_model, &$tags, $score) {
-		$TrueWordScore = 2;
-		$FalseWordScore = 0;
-		#what should this point to?
-		$python_file = "/Users/etaiklein/Localhost/tiltWorkspace/plugins/metadatagames/www/protected/modules/plugins/modules/weighting/components/DictCheck.py";
-		$is_word = "";
-		
-		foreach ($game->request->submissions as $submission) {
-			foreach ($tags as $image_id => $image_tags) {
-				foreach ($image_tags as $tag => $tag_info) {
-					
-							
-					# the path of the python file and the string
-					$command = "python $python_file $tag 2>&1";
-					#opens the connection between the python and php files
-					
-					#reads from the buffer
-					$pid = popen( $command,"r");
-					$is_word = substr(fread($pid, 256), 0, -1);				
-					pclose($pid);
-					
-					#add some error message
-					#will display true or false if the tag is an actual word based 
-					#on going through pyenchant library
-					#trigger_error($tag . $is_word);
-					
-						
-					#change the scores based on the outcome
-					switch($is_word){
-						case "True":
-							$this->addScore($tags[$image_id][$tag], $TrueWordScore);
-							$score = $score + $TrueWordScore;
-								
-							break;
+  public $enableOnInstall = true;
+  public $hasAdmin = true;
 
-						case "False":
-							$this->addScore($tags[$image_id][$tag], $FalseWordScore);
-							$score = $score + $FalseWordScore;
-							break;
-					}
-				}
-			}
-			break; // we expect only one submission.
-		}
-		return $score;
-	}
+  function score(&$game, &$game_model, &$tags, $score) {
+    $TrueWordScore = 2;
+    $FalseWordScore = 0;
+    #what should this point to?
+    $python_file = "/Users/etaiklein/Localhost/tiltWorkspace/plugins/metadatagames/www/protected/modules/plugins/modules/weighting/components/DictCheck.py";
+    $is_word = "";
+
+    foreach ($game->request->submissions as $submission) {
+      foreach ($tags as $image_id => $image_tags) {
+        foreach ($image_tags as $tag => $tag_info) {
+
+
+          # the path of the python file and the string
+          $command = "python $python_file $tag 2>&1";
+          #opens the connection between the python and php files
+
+          #reads from the buffer
+          $pid = popen( $command,"r");
+          $is_word = substr(fread($pid, 256), 0, -1);				
+          pclose($pid);
+
+	  #add some error message
+	  #will display true or false if the tag is an actual word based 
+	  #on going through pyenchant library
+	  #trigger_error($tag . $is_word);
+	
+	
+	  #change the scores based on the outcome
+	  switch($is_word){
+	    case "True":
+	      $this->addScore($tags[$image_id][$tag], $TrueWordScore);
+	      $score = $score + $TrueWordScore;
+	
+	      break;
+	
+	    case "False":
+	      $this->addScore($tags[$image_id][$tag], $FalseWordScore);
+	      $score = $score + $FalseWordScore;
+	      break;
+	  }
+        }
+      }
+    break; // we expect only one submission.
+    }
+  return $score;
+  }
 }
