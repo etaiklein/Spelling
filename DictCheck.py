1c7209e7f477f5ae05093ee22924323d5856fefb
etaiklein <etaiklein@gmail.com>
2013-06-25 18:59:42 -0300

    Updated spacing

--- DictCheck.py
+++ DictCheck.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 import sys
+#add your path here
+#sys.append()
 import nltk
 from nltk.corpus import wordnet as wn
 #SpellChecks a single item
 def Checkitem(item):
-    item = item[1]
+  item = item[1]
 
-    #checks if it is a word
-    if not wn.synsets(item):
-        return "False"
-    else:
-        return "True"
+  #checks if it is a word
+  if not wn.synsets(item):
+    return "False"
+  else:
+    return "True"
 
 print Checkitem(sys.argv)    
