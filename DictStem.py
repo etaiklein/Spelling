9861be1bb81dc18935c3cab7883edb86fbf554f7
etaiklein <etaiklein@gmail.com>
2013-06-25 01:56:10 -0300

    Updates Comments

--- DictStem.py
+++ DictStem.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 import sys
 import os
-
-#put the path of your enchant library here
 import nltk
 from nltk import WordNetLemmatizer
 
@@ -10,6 +8,7 @@ from nltk import WordNetLemmatizer
 def Check(mArray):
 
     #what am I checking?
+    #Taking the 2nd item in the array since popopen puts the file path as the first item.
     item = mArray[1]
     lmtzr = WordNetLemmatizer()
     item = lmtzr.lemmatize(item)
